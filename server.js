0b5ac6062a81bd52f49d3d77a19eb0fadf97bca9
Andres Aponte <andresaponte.f@gmail.com>
2021-06-19 12:31:30 -0400

    CSS now shows properly

--- server.js
+++ server.js
@@ -10,19 +10,20 @@ let notes = JSON.parse(fs.readFileSync(path.join(__dirname, './db/db.json')))
 
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
+app.use(express.static('./public'))
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')))
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 
-app.get('/api/notes', (req, res) => res.json(notes))
+app.get('/api/notes', (req, res) => res.json(notes));
 
 app.post('/api/notes', (req, res) => {
   let newNote = req.body
   notes.push(newNote)
   fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes));
   res.json(notes)
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Server now on port http://localhost:${PORT}`)
-})
+});
