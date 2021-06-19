1f189bdbee295b98f60fbea918619adc057aa410
Andres Aponte <andresaponte.f@gmail.com>
2021-06-19 13:25:31 -0400

    Final commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# README Generator
+----
+<a href="https://img.shields.io/badge/License-MIT-brightgreen"><img src="https://img.shields.io/badge/License-MIT-brightgreen"></a>
+## Table of Contents
+- [Description](#description)
+- [Contact Information](#contact-information)
+
+### Description
+Note taker allows you to write, save, and delete notes
+![screenshot](https://i.imgur.com/4ftNIEg.png)
+![GIF](https://i.imgur.com/51af1Lr.gif)
+
+
+### Contact-Information
+* [Github Profile](https://github.com/andresaponte22)
+* [Email](andresaponte.f@gmail.com)
--- server.js
+++ server.js
@@ -2,24 +2,23 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid')
 
 // app and port used
 const app = express()
 const PORT = process.env.PORT || 8000
-
-// storing database
-let notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/app/db.json')))
 
 // allows app to read our files
 app.use(express.urlencoded({extended : true}))
 app.use(express.json())
 app.use(express.static('./app/public'))
 
+// storing data base
+let notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/app/db.json')))
+
 // html routes
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/app/public/index.html')));
-app.get("*", (req, res)=> res.sendFile(path.join(__dirname,'/app/public/index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/app/public/notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './app/public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './app/public/notes.html')));
 app.get('/api/notes', (req, res) => res.json(notes));
 
 
@@ -28,18 +27,21 @@ app.post('/api/notes', (req, res) => {
   let newNote = req.body
   newNote.id = uuidv4()
   notes.push(newNote)
-  fs.writeFileSync(path.join(__dirname, '/app/db.json'), JSON.stringify(notes));
+  fs.writeFileSync(path.join(__dirname, './app/db.json'), JSON.stringify(notes));
   res.json(notes)
 });
+
 app.delete('/api/notes/:id', (req, res) => {
   const remove = req.params.id
   notes = notes.filter((value) => {
     return value.id !== remove
   })
-  fs.writeFileSync(path.join(__dirname, '/app/db.json'), JSON.stringify(notes))
+  fs.writeFileSync(path.join(__dirname, './app/db.json'), JSON.stringify(notes))
+  res.send('You deleted a note')
 });
 
 // server listener
+app.get("*", (req, res)=> res.sendFile(path.join(__dirname,'./app/public/index.html')));
 app.listen(PORT, () => {
   console.log(`Server now on port http://localhost:${PORT}`)
 });
